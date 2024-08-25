c55e35139dc2f0d02971c4a11ea0d1410c3066ee
webQbe <webcubesolutionslk@gmail.com>
2024-08-25 17:32:52 +0530

    ordered blog posts by date

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -11,6 +11,8 @@ from django.urls import reverse_lazy
 class HomeView(ListView):
     model = Post
     template_name = 'home.html'
+    ordering = ['-post_date'] 
+    #ordering = ['-id'] 
 
 class ArticleDetailView(DetailView):
     model = Post
