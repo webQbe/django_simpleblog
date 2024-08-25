b7245bfe92ae05ed19c379bf78d30ddda02e6471
webQbe <webcubesolutionslk@gmail.com>
2024-08-25 16:58:55 +0530

    post delete enabled

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 from .forms import PostForm, EditForm
+from django.urls import reverse_lazy
 
 # Create your views here.
 # def home(request):
@@ -26,6 +27,13 @@ class UpdatePostView(UpdateView):
     model = Post
     form_class = EditForm
     template_name = 'update_post.html'
-    #fields = ['title','title_tag','body']     
+    #fields = ['title','title_tag','body'] 
+
+class DeletePostView(DeleteView):
+    model = Post
+    template_name = 'delete_post.html'
+    success_url = reverse_lazy('home')
+
+
 
     
