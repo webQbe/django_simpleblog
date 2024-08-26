a960ef4599136ce2f4a53b700adf28d71c900645
webQbe <webcubesolutionslk@gmail.com>
2024-08-26 21:00:58 +0530

    added blog categories

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from .models import Post
+from .models import Post, Category
 from .forms import PostForm, EditForm
 from django.urls import reverse_lazy
 
@@ -23,6 +23,13 @@ class AddPostView(CreateView):
     form_class = PostForm
     template_name = 'add_post.html'
     # fields = '__all__'
+    # fields = ('title', 'body')
+
+class AddCategoryView(CreateView):
+    model = Category
+    #form_class = PostForm
+    template_name = 'add_category.html'
+    fields = '__all__'
     # fields = ('title', 'body')
 
 class UpdatePostView(UpdateView):
