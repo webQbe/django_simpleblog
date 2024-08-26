fa8983cc61c8f466e68e0e34e12bc601c015550d
webQbe <webcubesolutionslk@gmail.com>
2024-08-26 16:36:47 +0530

    added user authentication to add/edit/delete posts

--- myblog/members/urls.py
+++ myblog/members/urls.py
@@ -3,6 +3,5 @@ from .views import UserRegisterView
 
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='register'),
-    
-    
+ 
 ]
--- myblog/members/views.py
+++ myblog/members/views.py
@@ -3,6 +3,7 @@ from django.views import generic
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 
+
 class UserRegisterView(generic.CreateView):
     form_class = UserCreationForm
     template_name = 'registration/register.html'
