f20fb580086fe8afb890c575ed74e86de88cda01
webQbe <webcubesolutionslk@gmail.com>
2024-08-26 21:55:06 +0530

    category pages created

--- myblog/blog/urls.py
+++ myblog/blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 #from .import views
-from .views import HomeView, ArticleDetailView, AddPostView, UpdatePostView, DeletePostView, AddCategoryView
+from .views import HomeView, ArticleDetailView, AddPostView, UpdatePostView, DeletePostView, AddCategoryView, CategoryView
 
 urlpatterns = [
     # path('', views.home, name="home"),
@@ -10,4 +10,5 @@ urlpatterns = [
     path('add_category/', AddCategoryView.as_view(), name='add_category'),
     path('article/edit/<int:pk>', UpdatePostView.as_view(), name="update_post"),
     path('article/<int:pk>/remove', DeletePostView.as_view(), name="delete_post"),
+    path('category/<str:cats>/', CategoryView, name='category'),
 ]
--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -14,6 +14,11 @@ class HomeView(ListView):
     ordering = ['-post_date'] 
     #ordering = ['-id'] 
 
+def CategoryView(request, cats):
+    category_posts = Post.objects.filter(category=cats)
+    return render(request, 'categories.html', {'cats':cats.title(), 'category_posts':category_posts})
+
+
 class ArticleDetailView(DetailView):
     model = Post
     template_name = 'article_details.html'
