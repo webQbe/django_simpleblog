6ba122396465f4c2abdc993bd3476e1018b3721a
webQbe <webcubesolutionslk@gmail.com>
2024-08-27 20:41:09 +0530

    created an unlike button

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -11,7 +11,14 @@ from django.http import HttpResponseRedirect
 
 def LikeView(request, pk):
     post = get_object_or_404(Post, id=request.POST.get('post_id'))
-    post.likes.add(request.user)
+    liked = False
+    if post.likes.filter(id=request.user.id).exists():
+        post.likes.remove(request.user)
+        liked = False
+    else:
+        post.likes.add(request.user)
+        liked = True
+
     return HttpResponseRedirect(reverse('article-detail', args=[str(pk)]))
 
 
@@ -46,8 +53,14 @@ class ArticleDetailView(DetailView):
         
         stuff = get_object_or_404(Post, id=self.kwargs['pk'])
         total_likes = stuff.total_likes()
+
+        liked = False
+        if stuff.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
         context["cat_menu"] = cat_menu
         context["total_likes"] = total_likes
+        context["liked"] = liked
         return context 
     
 
