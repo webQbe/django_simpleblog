cf58bb8e4ba117fcc9468fe9670f94aa4079a4b1
webQbe <webcubesolutionslk@gmail.com>
2024-08-28 20:28:27 +0530

    added post snippet field

--- myblog/blog/forms.py
+++ myblog/blog/forms.py
@@ -12,7 +12,7 @@ for item in choices:
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'title_tag','author', 'category','body')
+        fields = ('title', 'title_tag','author', 'category','body', 'snippet')
 
         widgets = {
             'title' : forms.TextInput(attrs={'class':'form-control'}),
@@ -21,17 +21,19 @@ class PostForm(forms.ModelForm):
             #'author' : forms.Select(attrs={'class':'form-control'}),
             'category' : forms.Select(choices=choice_list, attrs={'class':'form-control'}),
             'body' : forms.Textarea(attrs={'class':'form-control'}),
+            'snippet' : forms.Textarea(attrs={'class':'form-control'}),
         }
 
 
 class EditForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'title_tag', 'body')
+        fields = ('title', 'title_tag', 'body', 'snippet')
 
         widgets = {
             'title' : forms.TextInput(attrs={'class':'form-control'}),
             'title_tag' : forms.TextInput(attrs={'class':'form-control'}),
             #'author' : forms.Select(attrs={'class':'form-control'}),
             'body' : forms.Textarea(attrs={'class':'form-control'}),
+            'snippet' : forms.Textarea(attrs={'class':'form-control'}),
         }
