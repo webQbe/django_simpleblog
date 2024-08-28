510af5ad91fefb6809845f260e66c9f2ddb65cca
webQbe <webcubesolutionslk@gmail.com>
2024-08-28 22:32:00 +0530

    change password page created

--- myblog/members/urls.py
+++ myblog/members/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import UserRegisterView, UserEditView
+from .views import UserRegisterView, UserEditView, PasswordsChangeView
+from . import views
 
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='register'),
     path('edit_profile/', UserEditView.as_view(), name='edit_profile'),
-
- 
+    path('password/', PasswordsChangeView.as_view(template_name='registration/change-password.html')),
+    path('password_success', views.password_success, name="password_success"),
 ]
--- myblog/members/views.py
+++ myblog/members/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from django.views import generic     
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
+from django.contrib.auth.views import PasswordChangeView
 from django.urls import reverse_lazy
-from .forms import SignUpForm, EditProfileForm
+from .forms import SignUpForm, EditProfileForm, PasswordChangingForm
+
+
+class PasswordsChangeView(PasswordChangeView):
+    form_class = PasswordChangingForm
+    success_url = reverse_lazy('password_success')
+
+
+def password_success(request):
+    return render(request, 'registration/password_success.html', {})
+
 
 
 class UserRegisterView(generic.CreateView):
