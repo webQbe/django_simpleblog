458351dff8ab7d121c4fa5db2168c2ad76f4b03c
webQbe <webcubesolutionslk@gmail.com>
2024-08-29 22:04:48 +0530

    user profile page created

--- myblog/members/urls.py
+++ myblog/members/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import UserRegisterView, UserEditView, PasswordsChangeView
+from .views import UserRegisterView, UserEditView, PasswordsChangeView, ShowProfilePageView
 from . import views
 
 urlpatterns = [
@@ -7,4 +7,5 @@ urlpatterns = [
     path('edit_profile/', UserEditView.as_view(), name='edit_profile'),
     path('password/', PasswordsChangeView.as_view(template_name='registration/change-password.html')),
     path('password_success', views.password_success, name="password_success"),
+    path('<int:pk>/profile/', ShowProfilePageView.as_view(), name="show_profile_page"),
 ]
