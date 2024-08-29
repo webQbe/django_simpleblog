81e151985a88c4e4ee3f16eb1f24419318c36397
webQbe <webcubesolutionslk@gmail.com>
2024-08-29 20:41:13 +0530

    added default profile pic with static files

--- myblog/myblog/settings.py
+++ myblog/myblog/settings.py
@@ -122,6 +122,10 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_URL = 'media/'
 
+STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static')
+]
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_DIRS = (
