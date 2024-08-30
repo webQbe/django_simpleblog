29142381b6c2051c5bbc13032c5572a911650c78
webQbe <webcubesolutionslk@gmail.com>
2024-08-30 12:27:28 +0530

    created edit profile page form

--- myblog/members/views.py
+++ myblog/members/views.py
@@ -8,6 +8,12 @@ from .forms import SignUpForm, EditProfileForm, PasswordChangingForm
 from blog.models import Profile
 
 
+class EditProfilePageView(generic.UpdateView):
+    model = Profile
+    template_name = 'registration/edit_profile_page.html'
+    fields = ['bio','profile_pic','website_url','facebook_url','twitter_url','instagram_url','pinterest_url']
+    success_url = reverse_lazy('home')
+
 class ShowProfilePageView(DetailView):
     model = Profile
     template_name = 'registration/user_profile.html'
